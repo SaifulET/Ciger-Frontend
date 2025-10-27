1819a75cc50ce51fee65bb80a4e9ed118ab8709a
SaifulET <si912999@gmail.com>
2025-10-27 06:01:55 +0600

    footer responsiveness

--- app/components/FooterSection/Footer.tsx
+++ app/components/FooterSection/Footer.tsx
@@ -8,25 +8,26 @@ import tiktokIcon from "@/public/TikTok.svg"
 import Image from "next/image";
 import logo from "@/public/logo1.svg"
 import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer className="bg-[#212121] w-full text-white px-8 md:px-16 py-16 flex flex-col md:gap-10">
+    <footer className="bg-[#212121] w-full text-white px-4 sm:px-8 lg:px-16 py-16 flex flex-col gap-10">
       {/* Main Container */}
-      <div className="w-full  flex flex-col gap-12">
+      <div className="w-full flex flex-col gap-12">
         {/* Top Section */}
-        <div className="flex flex-col md:flex-row justify-between gap-12 md:gap-[303px]">
+        <div className="flex flex-col lg:flex-row justify-between gap-8 lg:gap-4">
           {/* Left: Logo & Description */}
-          <div className="flex flex-col gap-12 w-full ">
+          <div className="flex flex-col gap-6 lg:gap-12 w-full max-w-md">
             <p className="text-white font-[600] text-[16px] leading-[24px] font-[Open_Sans]">
               The Finest in Tobacco, Vapes, and Lifestyle Accessories.
             </p>
-            <h2 className="text-white font-[Montserrat] font-semibold text-[40px] leading-[48px]">
-             <Image className=' ' src={logo} alt="Logo"width={220} height={220}/>
+            <h2 className="text-white font-[Montserrat] font-semibold">
+             <Image className='' src={logo} alt="Logo" width={220} height={220}/>
             </h2>
           </div>
 
           {/* Center: Quick Links */}
-          <div className="flex flex-col gap-4 w-full md:w-[123px]">
+          <div className="flex flex-col gap-4 w-full lg:w-auto">
             <h4 className="font-[Montserrat] font-semibold text-[18px] leading-[26px]">
               Quick Links
             </h4>
@@ -52,11 +53,11 @@ export default function Footer() {
           </div>
 
           {/* Right: Contact + Socials */}
-          <div className="flex flex-col  ">
+          <div className="flex flex-col gap-4 lg:gap-6">
             <Link href="/pages/contact">
-            <button className="bg-[#C9A040] text-[#0C0C0C] font-semibold px-8 py-3 rounded-xl hover:bg-[#d4ae56] transition">
-              Contact Us
-            </button>
+              <button className="bg-[#C9A040] text-[#0C0C0C] font-semibold px-6 py-3 rounded-xl hover:bg-[#d4ae56] transition lg:w-auto text-center">
+                Contact Us
+              </button>
             </Link>
 
             <div className="flex items-center gap-3">
@@ -75,23 +76,23 @@ export default function Footer() {
 
             <div className="flex items-center gap-6 mt-2">
               <a href="#" className="hover:opacity-80">
-                <Image src={fbIcon} width={20}height={20}alt="facebook"></Image>
+                <Image src={fbIcon} width={20} height={20} alt="facebook" />
               </a>
               <a href="#" className="hover:opacity-80">
-                <Image src={instaIcon} width={20}height={20}alt="instagram"></Image>
+                <Image src={instaIcon} width={20} height={20} alt="instagram" />
               </a>
               <a href="#" className="hover:opacity-80">
-                <Image src={youtubeIcon} width={20}height={20}alt="youtube"></Image>
+                <Image src={youtubeIcon} width={20} height={20} alt="youtube" />
               </a>
               <a href="#" className="hover:opacity-80">
-                <Image src={tiktokIcon} width={20}height={20}alt="tiktok"></Image>
+                <Image src={tiktokIcon} width={20} height={20} alt="tiktok" />
               </a>
             </div>
           </div>
         </div>
 
         {/* Bottom Text */}
-        <p className="text-center font-[Open_Sans] font-semibold text-[16px] leading-[24px]">
+        <p className="text-center font-[Open_Sans] font-semibold text-[16px] leading-[24px] mt-8">
           © 2025 All rights reserved to Smokenza
         </p>
       </div>
--- app/components/universalComponents/BlogCard.tsx
+++ app/components/universalComponents/BlogCard.tsx
@@ -18,7 +18,7 @@ const BlogCard: React.FC<BlogCardProps> = ({ product }) => {
   const { image, title, description, link } = product;
 
   return (
-    <div className="flex flex-col items-start   border border-[#B0B0B0] rounded-[12px] overflow-hidden bg-white ">
+    <div className="flex flex-col items-start mx-5 md:mx-0   border border-[#B0B0B0] rounded-[12px] overflow-hidden bg-white ">
       {/* Image */}
       <div className="relative w-full  bg-[#D9D9D9]">
         <Image
@@ -40,10 +40,10 @@ const BlogCard: React.FC<BlogCardProps> = ({ product }) => {
         </p>
 
         <Link
-              href="/pages/blog/1"
+              href="/pages/blog/1" className="w-full"
             >
-              <button className="flex justify-center items-center gap-2 w-60 md:w-80 h-[52px] bg-[#C9A040] rounded-[12px] hover:bg-[#B78E35] transition-colors">
-          <span className="text-[16px] leading-[24px] font-semibold font-[Open_Sans] text-[#0C0C0C]">
+              <button className="flex justify-center items-center w-full h-[52px] bg-[#C9A040] rounded-[12px] hover:bg-[#B78E35] transition-colors">
+          <span className="text-[16px] leading-[24px] font-semibold font-[Open_Sans] text-[#0C0C0C] w-full">
            
               View
             
