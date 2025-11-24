e6206899a12077bfd4c62198a4eeed67060620e6
SaifulET <si912999@gmail.com>
2025-11-25 05:31:58 +0600

    some changes

--- lib/axios.ts
+++ lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:"https://ciger-backend-2.onrender.com",
   // baseURL:"http://localhost:5001",
-  withCredentials: true, // optional
+   // optional
 });
 
 export default api;
