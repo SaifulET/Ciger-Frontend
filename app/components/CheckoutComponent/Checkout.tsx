e5f5654351d1ddb5362b9e559ab28441760f5cec
SaifulET <si912999@gmail.com>
2025-12-12 07:29:00 +0600

    update

--- app/components/CheckoutComponent/Checkout.tsx
+++ app/components/CheckoutComponent/Checkout.tsx
@@ -1363,6 +1363,7 @@ if(transactionid===0 && orderFailed==false)
             <div>
               <button
             id="checkout-button"
+            onClick={() => setIsAgeChecked(true)}
             className={`${isAgeChecked?"bg-green-600 hover:bg-green-700":"bg-yellow-600 hover:bg-yellow-700"} py-3 w-full rounded-md  transition-colors`}
           >
             {isAgeChecked ? (
