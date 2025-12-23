cccaf450c067a1fcc8e6a6dc0615778d490a6fbf
SaifulET <si912999@gmail.com>
2025-12-24 04:23:34 +0600

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
-
 import type { Metadata } from "next";
 import { ReactNode } from "react";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -12,20 +10,37 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://smokenza.com"),
+
   title: {
-    default: "Smokenza: Buy Premium Cigars, Hookah , Vapes & More",
+    default: "Smokenza: Buy Premium Cigars, Hookah, Vapes & More",
     template: "%s | Smokenza",
   },
-  description: "Shop premium cigars, hookah, nicotine vapes & accessories online. Fast delivery & secure checkout.",
-  metadataBase: new URL("https://smokenza.com"),
+
+  description:
+    "Shop premium cigars, hookah, nicotine vapes & accessories online. Fast delivery & secure checkout.",
+
+  icons: {
+    icon: [
+      {
+        url: "/favicon.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+    ],
+    apple: "/favicon.png",
+    shortcut: "/favicon.png",
+  },
+
   openGraph: {
-    title: "Smokenza: Buy Premium Cigars , Vapes & more",
-    description: "Shop premium cigars, hookah & nicotine vapes online with fast delivery.",
+    title: "Smokenza: Buy Premium Cigars, Vapes & More",
+    description:
+      "Shop premium cigars, hookah & nicotine vapes online with fast delivery.",
     url: "https://smokenza.com",
     siteName: "Smokenza",
     images: [
       {
-        url: "https://smokenza.com/og-image.jpg",
+        url: "/og-image.jpg",
         width: 1200,
         height: 630,
       },
@@ -34,20 +49,16 @@ export const metadata: Metadata = {
   },
 };
 
-
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
-  <html lang="en" className={poppins.className} suppressHydrationWarning>
-   
-    <body suppressHydrationWarning>
-      <div  className="bg-white text-[#212121]">
-          
-        
-        {children}
-        
-      </div>
-
-    </body>
-  </html>
-);
+    <html lang="en" className={poppins.className} suppressHydrationWarning>
+      <body suppressHydrationWarning>
+        <div className="bg-white text-[#212121]">{children}</div>
+      </body>
+    </html>
+  );
 }
